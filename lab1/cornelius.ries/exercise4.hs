0cc1c7475b4f72ac87fefc28b27230653da58ad6
Cornelius Ries <cornelius.ries@posteo.net>
2017-09-08 07:25:16 +0200

    lab1/ex4 ToDos

--- lab1/cornelius.ries/exercise4.hs
+++ lab1/cornelius.ries/exercise4.hs
@@ -19,3 +19,29 @@ isReversalPrime n = prime(reversal(n))
 
 reversalPrimes :: [Integer]
 reversalPrimes = takeWhile(<10000) (filter isReversalPrime primes)
+
+-- Test
+
+reversalTest :: Integer -> Bool
+reversalTest n = reversal(reversal n) == n
+
+testReversal = quickCheck(reversalTest)
+
+{-
+
+The reversal function converts the Integer into a String
+(:t read ------ read :: Read a => String -> a),
+reverses the String and tries to parse the result as an Integer again.
+
+Cases were this will not work:
+
+- For negative numbers this would result in trying to parse eg: -5 -> 5-
+  This will lead to a parse error.
+
+- For numbers ending with a zero this will lead to eg: 1000 -> 0001
+  This will result in a failed test as the reversed result is 1.
+
+In our use case the function does work,
+because we are only working with primenumbers.
+
+-}
--- lab1/exercise4.hs
+++ lab1/exercise4.hs
@@ -15,5 +15,28 @@ predicate n = n > 0 && isPrime n && isPrime (reversal n)
 findPrimesIn1000 :: [Integer]
 findPrimesIn1000 = filter predicate [1..10000]
 
-{--TODO: Implement a test for reversal (reverse twice and check its the same) --}
-{--TODO: Argue why its hard to implement a test --}
+-- Test
+
+reversalTest :: Integer -> Bool
+reversalTest n = reversal(reversal n) == n
+
+testReversal = quickCheck(reversalTest)
+
+{-
+
+The reversal function converts the Integer into a String
+(:t read ------ read :: Read a => String -> a),
+reverses the String and tries to parse the result as an Integer again.
+
+Cases were this will not work:
+
+- For negative numbers this would result in trying to parse eg: -5 -> 5-
+  This will lead to a parse error.
+
+- For numbers ending with a zero this will lead to eg: 1000 -> 0001
+  This will result in a failed test as the reversed result is 1.
+
+In our use case the function does work,
+because we are only working with primenumbers.
+
+-}
