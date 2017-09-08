594758d6fafb1030b85706b6223a9adcd9ac7ab1
Cornelius Ries <cornelius.ries@posteo.net>
2017-09-08 21:11:59 +0200

    ex6+ex7

--- lab1/cornelius.ries/exercise6.hs
+++ lab1/cornelius.ries/exercise6.hs
@@ -1,8 +1,6 @@
 module Lab1 where
-import Data.List
-import Test.QuickCheck
 
- -- Time
+ -- Time 1h
 prime :: Integer -> Bool
 prime n = n > 1 && all (\ x -> rem n x /= 0) xs
    where xs = takeWhile (\ y -> y^2 <= n) primes
@@ -10,11 +8,20 @@ prime n = n > 1 && all (\ x -> rem n x /= 0) xs
 primes :: [Integer]
 primes = 2 : filter prime [3..]
 
-{-
-counterexamples :: Int -> Integer --[([Integer],Integer)]
-counterexamples n |
-  otherwise = prime(product(take n primes) + 1) : counterexamples(n)
+counterexamples :: [Integer] -> Int -> Integer --[([Integer],Integer)]
+counterexamples l n
+  | prime(product(take n primes) + 1) = counterexamples l (n + 1)
+  | otherwise = product(take n primes) + 1
 
-getNotPrimes :: Int -> [Integer]
-getNotPrimes n = counterexamples not $ primes
--}
+counterexamples2 :: [Integer] -> Int -> [([Integer],Integer)]
+counterexamples2 l n
+  | prime(product(take n primes) + 1) = counterexamples2 l (n + 1)
+  | otherwise = [(take n primes , product(take n primes) + 1)]
+
+counterexamples3 :: Int -> ([Integer],Integer)
+counterexamples3 n
+  | not $ prime(product(take n primes) + 1) = (take n primes, product(take n primes) + 1)
+  | otherwise = ([], 0)
+
+generateCounters :: [([Integer],Integer)]
+generateCounters = filter (\m -> m /= ([], 0)) (map counterexamples3 [1..])
--- lab1/cornelius.ries/exercise7.hs
+++ lab1/cornelius.ries/exercise7.hs
@@ -2,4 +2,53 @@ module Lab1 where
 import Data.List
 import Test.QuickCheck
 
- -- Time
+ -- Time 3h
+
+digits :: Integer -> [Integer]
+digits = map (read . (:[])) . show
+
+calcValue :: Integer -> Integer
+calcValue x
+  | x * 2 > 9 = x*2-9
+  | otherwise = x*2
+
+calcEverySecond :: [Integer] -> [Integer]
+calcEverySecond [] = []
+calcEverySecond [x] = [x]
+calcEverySecond (x:y:xs) = x : calcValue y : calcEverySecond xs
+
+luhn :: Integer -> Bool
+luhn i = sum(s) `mod` 10 == 0
+  where d = digits i
+        r = reverse d
+        s = calcEverySecond r
+
+startsWithS :: String -> String -> Bool
+startsWithS [] [] = True
+startsWithS _ [] = True
+startsWithS [] _ = False
+startsWithS (x:xs) (y:ys) = x == y && startsWithS xs ys
+
+startsWith :: Integer -> Integer -> Bool
+startsWith x y = startsWithS (show x) (show y)
+
+checkPrefixes :: [Integer] -> Integer -> Bool
+checkPrefixes [] _ = False
+checkPrefixes (x:xs) n = startsWith n x || checkPrefixes xs n
+
+checkLengths :: [Integer] -> Integer -> Bool
+checkLengths l n = toInteger(length(digits n)) `elem` l
+
+isAmericanExpress, isMaster, isVisa :: Integer -> Bool
+
+isAmericanExpress n = checkLengths validLengths n && checkPrefixes validPrefixes n && luhn n
+  where validLengths = [15]
+        validPrefixes = [34,37]
+
+isMaster n = checkLengths validLengths n && checkPrefixes validPrefixes n && luhn n
+  where validLengths = [16]
+        validPrefixes = [51,52,53,54,55] ++ [222100..272099]
+
+isVisa n = checkLengths validLengths n && checkPrefixes validPrefixes n && luhn n
+  where validLengths = [13,16,19]
+        validPrefixes = [4]
