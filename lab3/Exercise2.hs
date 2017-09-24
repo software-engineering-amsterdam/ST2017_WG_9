0800a6aeb7cf8150a6a0fa5be47868e18474e975
piotrkosytorz <piotr.kosytorz@gmail.com>
2017-09-24 17:13:20 +0200

    1-4

--- lab3/Exercise2.hs
+++ lab3/Exercise2.hs
@@ -2,7 +2,6 @@ module Exercise2 where
 
 import Data.List
 import System.Random
-import Test.QuickCheck
 import Lecture3
 
 {--
@@ -26,22 +25,26 @@ import Lecture3
 
 --}
 
+
 test :: Form -> Bool
 test f = f == head (parse $ show f) -- parse returns a one-element list of formulas
 
 {--
-  For testing the following formulas can be used
+  For testing the following formulas can be used:
 --}
 
 testFormula1 = form1
 testFormula2 = form2
 testFormula3 = form3
-testFormula4 = form4
+testFormula4 = Equiv (Neg p) (p)
 testFormula5 = Equiv form1 form3
 testFormula6 = Cnj [form1,form2]
 testFormula7 = Dsj [form2, form3]
 testFormula8 = Impl form1 form2
 
+{--
+  A collecting list of all formulas to be tested:
+--}
 allFormulas = [testFormula1,
   testFormula2,
   testFormula3,
@@ -52,4 +55,13 @@ allFormulas = [testFormula1,
   testFormula8]
 
 detailedTest = map (\f -> test f) allFormulas
+{-- Resutls:
+  *Lab3Exercise2> detailedTest
+  [True,True,True,True,True,True,True,True]
+--}
+
 yesNoTest = all (\f -> test f) allFormulas
+{-- Results:
+  *Lab3Exercise2> yesNoTest
+  True
+--}
--- lab3/piotr.kosytorz/exercise3.hs
+++ lab3/piotr.kosytorz/exercise3.hs
@@ -1,4 +1,4 @@
-module Lab3Exercise3 where
+module Exercise3 where
 
 import Data.List
 import System.Random
@@ -6,34 +6,62 @@ import Test.QuickCheck
 import Lecture3
 
 {--
-  TODO:
-    - time indication
-    - report
-    - write test generator with ex4: test if the truth tables are the same
-    - (optionally) write test for CNF (is it really CNF)
+  Time spent: 2h
 --}
 
+{--
+  There are multiple ways to generate a CNF form from a propositional logic formula.
+  The most obvious and direct one would be to implement the standard algorithm that
+  involves usage og the double negative law, De Morgan's laws, and the distributive law.
+
+
+  Another way is the method that we've used in the workshop and that is based on
+  retrieving a CNF form from furmula's truth table. I've decided to implement this
+  one because it is correct and easy to follow. It has a big disadvantage though:
+  its memory complexicity. Nevertheless is still a correct method and easy to
+  implement.
+
+--}
+
+-- | a test for tautologies
 tautology :: Form -> Bool
 tautology formx = all (\x -> evl x formx) $ allVals formx
 
--- extracts valuations which give False as result
+-- | extracts valuations which give False as result (comp. https://math.stackexchange.com/questions/636119/find-dnf-and-cnf-of-an-expression#637445)
 extractFalseResults :: Form -> [Valuation]
 extractFalseResults f = filter (\x -> not $ evl x f) $ allVals f
 
--- Valuations to disjunction of literals
+-- | valuations to disjunction of literals
+{--
+  The first step is to construct the disjuntive sub-lists based on values from
+  the truth table (the rows),
+  So each row that evaluates a False result is taken and with v2dl the single
+  propositions are negated whenever they have a True value, next all obtained
+  literals are set in a disjuntion.
+--}
 v2dl :: Valuation -> Form
 v2dl (x:xs)
-  | xs == [] = if snd x then Neg (Prop (fst x)) else Prop (fst x)
-  | otherwise = if snd x then Dsj [Neg (Prop (fst x)), v2dl xs] else Dsj [Prop (fst x), v2dl xs]
+  | xs == [] = if snd x then Neg (Prop (fst x)) else Prop (fst x) -- the last element on the list doesn't have to be set in a Dsj list, we'll take care f that earlier
+  | otherwise = if snd x then Dsj [Neg (Prop (fst x)), v2dl xs] else Dsj [Prop (fst x), v2dl xs] -- not reached the last element yet, which means that the literals have to be created and the Dsj list has to be expanded
 
 -- Conjunction of formulas
+{--
+  The last step is to put everything togerher, so to make a Conjunction of all rows:
+--}
 cf :: [Form] -> Form
 cf (x:xs)
   | xs == [] = x
   | otherwise = Cnj [x, cf xs]
 
 -- CNF from
+{--
+  Finaly the CNF form puts all the helpers together in one set of operations,
+  but fist checks if the formula is a tautology.
+  We've agreed to use p ∨ ¬p as the basic formula for tautology.
+  The Form data structure given in Lecture3.hs doesn't provide True and False
+  as possible elements of the language.
+--}
 cnf :: Form -> Form
 cnf f
-  | tautology f = error "Given formula is a tautology."
+  | tautology f = Dsj [p, Neg p]
   | otherwise = cf $ map (\x -> v2dl x) $ extractFalseResults f
