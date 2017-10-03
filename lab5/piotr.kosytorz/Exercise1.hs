798cad1d943b764cc7d90bb94fc2464351e1175f
piotrkosytorz <piotr.kosytorz@gmail.com>
2017-10-03 14:36:52 +0200

    puzzle - ex1

--- lab5/piotr.kosytorz/Exercise1.hs
+++ lab5/piotr.kosytorz/Exercise1.hs
@@ -3,7 +3,11 @@ module Exercise1 where
 
 -- | Exercise1
 -- | ===========================================================================
--- | Time spent: 1h30m
+-- | Time spent: 2h
+
+-- | Note: modifications are marked and explained
+-- | Note2: solution for the puzzle at the bottom of the script or by running
+-- | main
 
 import Data.List
 import System.Random
@@ -339,6 +343,21 @@ example5 = [[1,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,8,0],
             [0,0,0,0,0,0,0,0,9]]
 
+-- | Puzzle to solve (from exercise)
+-- | ===========================================================================
+puzzle :: Grid
+puzzle = [[0,0,0,3,0,0,0,0,0],
+          [0,0,0,7,0,0,3,0,0],
+          [2,0,0,0,0,0,0,0,8],
+          [0,0,6,0,0,5,0,0,0],
+          [0,9,1,6,0,0,0,0,0],
+          [3,0,0,0,7,1,2,0,0],
+          [0,0,0,0,0,0,0,3,1],
+          [0,8,0,0,4,0,0,0,0],
+          [0,0,2,0,0,0,0,0,0]]
+-- | ===========================================================================
+-- | end puzzle
+
 emptyN :: Node
 emptyN = (\ _ -> 0,constraints (\ _ -> 0))
 
@@ -428,8 +447,21 @@ genProblem n = do ys <- randomize xs
                   return (minimalize n ys)
    where xs = filledPositions (fst n)
 
-main :: IO ()
-main = do [r] <- rsolveNs [emptyN]
-          showNode r
-          s  <- genProblem r
-          showNode s
+main :: IO[()]
+main = solveAndShow puzzle
+
+-- | Solution for the puzzle
+-- | ===========================================================================
+-- +-------+-------+-------+
+-- | 4 7 8 | 3 9 2 | 6 1 5 |
+-- | 6 1 9 | 7 5 8 | 3 2 4 |
+-- | 2 3 5 | 4 1 6 | 9 7 8 |
+-- +-------+-------+-------+
+-- | 7 2 6 | 8 3 5 | 1 4 9 |
+-- | 8 9 1 | 6 2 4 | 7 5 3 |
+-- | 3 5 4 | 9 7 1 | 2 8 6 |
+-- +-------+-------+-------+
+-- | 5 6 7 | 2 8 9 | 4 3 1 |
+-- | 9 8 3 | 1 4 7 | 5 6 2 |
+-- | 1 4 2 | 5 6 3 | 8 9 7 |
+-- +-------+-------+-------+
