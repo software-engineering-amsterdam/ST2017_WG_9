ca0c90494fde37c680f779ce4f1acd389b5ce4b1
piotrkosytorz <piotr.kosytorz@gmail.com>
2017-10-09 23:50:59 +0200

    ex1 - fixed

--- lab6/piotr.kosytorz/Exercise1.hs
+++ lab6/piotr.kosytorz/Exercise1.hs
@@ -5,22 +5,29 @@ import System.Random
 import Math.NumberTheory.Logarithms
 import Lecture6
 
--- integerLog2
-
--- | infinite list of powers of two
-p2 = iterate (*2) 2
-
 -- | subpower of 2 = length of the list = integerLog2
 
--- | x^33 mod 5 = (x^32 mod 5)*(x^1 mod 5)
+-- | x^33 mod 5 = [(x^32 mod 5)*(x^1 mod 5)] mod 5
+-- | \......../    \........../\........../
+-- |   result        part1       part2
+-- |
 -- | explanation:
 -- | number of recurrences: integerLog2 33 = 5
 -- | what is left: 33-(2^5) = 33-32 = 1
 
-buildPow2 :: Integer -> Int -> Integer -> Integer
-buildPow2 x i n
-  | i > 0 = buildPow2 (x^2 `mod` n) (i-1) n
-  | otherwise = x
-
 myExM :: Integer -> Integer -> Integer -> Integer
-myExM x y n = ((buildPow2 x (integerLog2 y) n) * (x^(y-2^(integerLog2 y)) `mod` n)) `mod` n
+myExM _ 0 _ = 1
+myExM x y n = (
+      -- part1
+      (pow2mod x (integerLog2 y) n)
+      *
+      -- part2
+      -- (x^(y-2^(integerLog2 y)) `mod` n))
+      (myExM x (y-2^(integerLog2 y)) n)
+    )
+    -- mod n
+    `mod` n
+    where
+    pow2mod x i n -- | part1 returns: x^2^i mod n
+      | i > 0 = pow2mod (x^2 `mod` n) (i-1) n
+      | otherwise = x
