60ac19d6b029340fe878d8303683b23605dbae46
Simon Schneider <s.schneider@zwei14.de>
2017-10-15 16:24:43 +0200

    Add exercise 2 and 3

--- lab6/Exercise3.hs
+++ lab6/Exercise3.hs
@@ -13,17 +13,26 @@ Answer:
 See composites' for infinite,
 compositesFinite for finite list:
 
-*Exercise3> compositesFinite 10
-[4,6,8,9,10]
+*Exercise3> compositesFinite 20
+[4,6,8,9,10,12,14,15,16,18,20]
+
+*Exercise3> primes
+[2,3,5,7,11,13,17,19...]
 
 Every integer is composite as long as it is not a prime number. However, there is
 one exception to this rule. The number 1 is not a prime number (does not have two
 diffrent divisors) nor a composite (a whole number that can be divided by numbers
 other than 1 or itself)
 
+Example:
+*Exercise3> sort((takeWhile (<=20) primes) ++ compositesFinite 20 ++ [1]) == [1..20]
+True
+
 -------------------------------------------------------------------------------}
 
 import Lecture6
+
+import Data.List
 
 -- One is neither prime nor composite
 isComposite :: Integer -> Bool
